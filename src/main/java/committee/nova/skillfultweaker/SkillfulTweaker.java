74673780b202ebd3507a3a87188183798695bc9c
MikhailTapio <mikhailtapio13468@163.com>
2023-03-29 10:38:05 +0800

    Dependencies

--- src/main/java/committee/nova/skillfultweaker/SkillfulTweaker.java
+++ src/main/java/committee/nova/skillfultweaker/SkillfulTweaker.java
@@ -2,6 +2,6 @@ package committee.nova.skillfultweaker;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = "skillfultweaker", useMetadata = true)
+@Mod(modid = "skillfultweaker", useMetadata = true, dependencies = "required-after:skillful@[0.0.3.3,);required-after:crafttweaker")
 public class SkillfulTweaker {
 }
