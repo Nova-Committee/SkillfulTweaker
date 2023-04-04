43c8510bd99c320b5f232b093542ef377cade2a6
MikhailTapio <mikhailtapio13468@163.com>
2023-04-04 23:19:42 +0800

    Add "clearSkillInfoCache"

--- src/main/java/committee/nova/skillfultweaker/compat/expansion/SkillfulPlayer.java
+++ src/main/java/committee/nova/skillfultweaker/compat/expansion/SkillfulPlayer.java
@@ -5,6 +5,9 @@ import committee.nova.skillfultweaker.compat.skill.objects.Skill;
 import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.minecraft.CraftTweakerMC;
 import crafttweaker.api.player.IPlayer;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ResourceLocation;
 import stanhebben.zenscript.annotations.ZenExpansion;
 import stanhebben.zenscript.annotations.ZenMethod;
@@ -15,5 +18,14 @@ public class SkillfulPlayer {
     @ZenMethod
     public static Skill getSkillStat(IPlayer player, String id) {
         return new Skill(Utilities.getPlayerSkillStat(CraftTweakerMC.getPlayer(player), new ResourceLocation(id.contains(":") ? id : "crt:" + id)));
+    }
+
+    @ZenMethod
+    public static void clearSkillInfoCache(IPlayer player) {
+        final EntityPlayer p = CraftTweakerMC.getPlayer(player);
+        if (!(p instanceof EntityPlayerMP)) return;
+        final MinecraftServer server = p.getServer();
+        if (server == null) return;
+        server.commandManager.executeCommand(p, "/skillful clearinfo");
     }
 }
